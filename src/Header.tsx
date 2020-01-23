ac29beb2add9adb27bbf9495f3014923b1a8419d
Steven Bruce <steven.bruce.au@gmail.com>
2020-01-23 22:46:36 +1100

    Added GitHub link

--- src/Header.tsx
+++ src/Header.tsx
@@ -7,6 +7,9 @@ const Header: React.SFC<{}> = (props) => {
   	<div className={styles.header}>
   		<div className={styles.title}>Fraction Ninja</div>
   		<div className={styles.description}>Learn about fractions and decimals</div>
+	    <a href="https://github.com/steven-bruce-au/Fraction-Ninja">
+	    	<img className={styles.github} src="./GitHub_Logo_White.png"/>
+	    </a>
     </div>);
 }
 
